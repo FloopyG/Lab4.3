5a3aa792942ff8dd3a0a45ba9f4affa974ee6454
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-10-24 20:25:02 +0300

    Initial commit

---
+++ Lab4.3.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double a, b, x, c, xp, xk, dx, i, F;
+
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+
+    cout << fixed;
+    cout << "------------------" << endl;
+    cout << "|" << setw(3) << "N" << "  |" << setw(5) << "F" << "     |" << endl;
+    cout << "------------------" << endl;
+
+    i = 1;
+    x = xp;
+    while (x <= xk) {
+        if (x < 0.6 && b + c != 0)
+            F = a * pow(x, 2) + pow(b, 2) + c;
+        else
+            if (x > 0.6 && b + c == 0)
+                F = (x - a) / (x - c);
+            else
+                F = x / c + x / a;
+
+
+        cout << "|" << setw(3) << setprecision(0) << i << "  |" << setw(7) << setprecision(2) << F << "   |" << endl;
+
+        i++;
+        x += dx;
+    };
+    cout << "------------------" << endl;
+
+
+    system("pause");
+    cin.get();
+    return 0;
+}
